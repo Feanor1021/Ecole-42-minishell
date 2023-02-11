1b2e89557cf6c02d87f295e4cd0a2e50fda012f6
Feanor1021 <furkanyardimci1006@gmail.com>
2023-02-11 19:29:10 +0300

    File deleted

--- Ecole-42-minishell/README.md
+++
@@ -1,3 +0,0 @@
-# Minishell
-
-A shell language project
--- Minishell/main.c
+++ Minishell/main.c
@@ -2,11 +2,6 @@
 
 int main(int argc, char **argv)
 {
-    if (argc < 3)
-    {
-        fprintf(stderr, "usage: lsgrep arg1 arg2\n");
-        exit(1);
-    }
     int tempin = dup(0);
     int tempout = dup(1);
 
@@ -14,47 +9,10 @@ int main(int argc, char **argv)
     pipe(fdpipe);
 
     dup2(fdpipe[1], 1);
-    close(fdpipe[1]);
-
-    int ret = fork();
-
-    if (ret == 0)
-    {
-        close(fdpipe[0]);
-        char *args[3];
-        args[0] = "ls";
-        args[1] = "-la";
-        args[2] = NULL;
-        execvp(args[0], args);
-        perror("execvp");
-        _exit(1);
-    }
-
     dup2(fdpipe[0], 0);
-    close(fdpipe[0]);
-
-    int fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-
-    if (fd < 0)
-    {
-        perror("open");
-        exit(1);
-    }
-    dup2(fd, 1);
-    close(fd);
-
-    ret = fork();
-    if (ret == 0)
-    {
-        char *args[3];
-        args[0] = "grep";
-        args[1] = argv[1];
-        args[2] = NULL;
-        execvp(args[0], args);
-        perror("execvp");
-        _exit(1);
-    }
-    dup2(0, tempin);
+    printf("seaqw\n");
+    char a[15];
+    fgets(a, 15, 0);
     dup2(1, tempout);
-    waitpid(-1, NULL, 0);
+    write(1, a, 15);
 }
