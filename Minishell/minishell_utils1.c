ff4e640b746df3941f012e4f91e19c4363e3778e
Feanor1021 <furkanyardimci1006@gmail.com>
2023-02-23 22:58:45 +0300

    new is_delimitter, is_quote, and general purpose addarr functions added

--- Minishell/minishell_utils1.c
+++ Minishell/minishell_utils1.c
@@ -2,8 +2,8 @@
 
 char **ft_copyarr_str(char **arr)
 {
-    char    **ret;
-    int     i;
+    char **ret;
+    int i;
 
     ret = ft_calloc(sizeof(char *), ft_arrlen((void **)arr) + 1);
     i = 0;
@@ -15,9 +15,38 @@ char **ft_copyarr_str(char **arr)
 int ft_arrlen(void **arr)
 {
     int len;
-    
+
     len = 0;
     while (arr && arr[len])
         len++;
     return (len);
+}
+
+void ft_addarr(void ***arr, void *new)
+{
+    void **ret;
+    int i;
+    int k;
+
+    ret = ft_calloc(sizeof(void *), ft_arrlen((void **)*arr) + 2);
+    i = 0;
+    k = 0;
+    while (*arr && (*arr)[k])
+        ret[i++] = (*arr)[k++];
+    ret[i] = new;
+    free(*arr);
+    (*arr) = ret;
+}
+
+int ft_isdelimitter(char chr)
+{
+    return (ft_isspace(chr) ||
+            chr == '<' ||
+            chr == '>' ||
+            chr == '|');
+}
+
+int ft_isquote(char chr)
+{
+    return (chr == '"' || chr == '\'');
 }
