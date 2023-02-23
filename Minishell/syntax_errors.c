9624b7bf511bc9450027fce3fcebb42e333075de
Feanor1021 <furkanyardimci1006@gmail.com>
2023-02-24 01:26:21 +0300

    function for syntax errors added

---
+++ Minishell/syntax_errors.c
@@ -0,0 +1,14 @@
+#include "minishell.h"
+
+void ft_syntaxerror(t_token *token)
+{
+    if (token)
+    {
+        ft_putstr_fd("bash: syntax error near unexpected token `", 2);
+        ft_putstr_fd(token->data, 2);
+        ft_putstr_fd("'\n", 2);
+    }
+    else
+        ft_putstr_fd("bash: sytax error near unexpected token `newline'", 2);
+    g_shell->return_code = 2; //?
+}
