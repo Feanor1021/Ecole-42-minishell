b35346dc1beb8580613dfbc934035fcb43364677
Feanor1021 <furkanyardimci1006@gmail.com>
2023-02-24 23:40:40 +0300

    parsecommand function finished and parsetoken added

--- Minishell/parsepipelines.c
+++ Minishell/parsepipelines.c
@@ -1,30 +1,30 @@
 #include "minishell.h"
 
-static t_pipeline	*parsepipeline(t_token **tokens, int start, int end)
+static int findend(t_token **tokens, int start, int end)
+{
+    while (start < end && tokens[start])
+        start++;
+    return (start);
+}
+
+static t_pipeline *parsepipeline(t_token **tokens, int start, int end)
 {
     t_pipeline *ret;
 
-    if((char)tokens[0]->data[0] == '|')
+    if ((char)tokens[0]->data[0] == '|')
     {
         ft_syntaxerror(*tokens);
         return (NULL);
     }
     ret = ft_calloc(sizeof(t_pipeline), 1);
     ret->commands = ft_parsecommands(tokens, start, end);
-    // if(!ret->commands)
-    // {
-    //     ft_freearr_command(ret->commands);
-    //     free(ret);
-    //     return (NULL);
-    // }
-}
-
-
-static int findend(t_token **tokens, int start, int end)
-{
-    while (start < end && tokens[start])
-        start++;
-    return (start);
+    if (!ret->commands)
+    {
+        ft_free_arr_command(ret->commands);
+        free(ret);
+        return (NULL);
+    }
+    return ret;
 }
 
 t_pipeline **ft_parsepipelines(t_token **tokens, int start, int end)
@@ -41,3 +41,16 @@ t_pipeline **ft_parsepipelines(t_token **tokens, int start, int end)
         pipeline = parsepipeline(tokens, start, i);
     }
 }
+
+// int j = 0, k = 0;
+// while ((pipeline->commands)[k])
+// {
+//     j = 0;
+//     printf("\n%s\n", (pipeline->commands)[k]->command);
+//     while (((pipeline->commands)[k]->arguments)[j])
+//     {
+//         printf("%s ", ((pipeline->commands)[k]->arguments)[j]);
+//         j++;
+//     }
+//     k++;
+// }
