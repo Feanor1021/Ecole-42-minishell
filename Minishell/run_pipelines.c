99c8bcabd25ae7e70fa240c3c403976ad75c9a3d
Feanor1021 <furkanyardimci1006@gmail.com>
2023-02-28 20:59:10 +0300

    function for running the command line in pipes added

---
+++ Minishell/run_pipelines.c
@@ -0,0 +1,11 @@
+#include "minishell.h"
+
+void ft_runpipelines(t_pipeline *pipes)
+{
+    g_shell->i_flag = 1;
+    if (!get_heredoc(pipes))
+        return;
+    if (pipes)
+        // run_commands(pipes->commands);
+        g_shell->i_flag = 0;
+}
