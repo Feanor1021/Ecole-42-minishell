02beebf04a7f97e8e7de5bd75d0de1f5c3c462b8
Feanor1021 <furkanyardimci1006@gmail.com>
2023-03-12 18:15:20 +0300

    Error = command it self considered as argument too ex "echo hi, command = echo, arguments = echo, hi" . This error fixed. Commands doesn't considered as argument.

--- Minishell/parsecommands_utils.c
+++ Minishell/parsecommands_utils.c
@@ -1,23 +1,23 @@
 #include "minishell.h"
 
-int ft_parsewordtoken(t_command *cmd, t_token **tokens, int i)
-{
-    if (!cmd->command)
-        cmd->command = ft_strdup(tokens[i]->data);
-    ft_add_arr_str(&(cmd->arguments), ft_strdup(tokens[i]->data));
-    return (1);
-}
-
 // int ft_parsewordtoken(t_command *cmd, t_token **tokens, int i)
 // {
 //     if (!cmd->command)
-//     {
 //         cmd->command = ft_strdup(tokens[i]->data);
-//         return 1;
-//     }
-//      ft_add_arr_str(&(cmd->arguments), ft_strdup(tokens[i]->data));
-//     return 1;
+//     ft_add_arr_str(&(cmd->arguments), ft_strdup(tokens[i]->data));
+//     return (1);
 // }
+
+int ft_parsewordtoken(t_command *cmd, t_token **tokens, int i)
+{
+    if (!cmd->command)
+    {
+        cmd->command = ft_strdup(tokens[i]->data);
+        return 1;
+    }
+    ft_add_arr_str(&(cmd->arguments), ft_strdup(tokens[i]->data));
+    return 1;
+}
 
 int ft_parse_heredoc(t_command *cmd, t_token **tokens, int *i)
 {
